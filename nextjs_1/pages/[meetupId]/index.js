749731cf162e46ded4381f0c57f3f99798aa8cce
JangHyuk00 <yim0823@hotmail.com>
2023-01-24 17:37:49 +0900

    pages settings and the basic use of getStaticProps

---
+++ nextjs_1/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+const MeetupDetail = () => {
+  return (
+    <MeetupDetails
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="A First Meetup"
+      address="Some City"
+      description="A First Description"
+    />
+  );
+};
+
+export default MeetupDetail;
--- nextjs_1/pages/index.js
+++ nextjs_1/pages/index.js
@@ -1,8 +1,32 @@
+import MeetupList from "../components/meetups/MeetupList"
 
-const HomePage = () => {
-    return(
-        <h1>The Home Page</h1>
-    )
+const DUMMY_MEETUPS = [
+    {
+        id: "m1",
+        title: "A First Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address: "some address 1, 12345 some city",
+        description: "This is a First meetup"
+    },
+    {
+        id: "m2",
+        title: "A Second Meetup",
+        image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+        address: "some address 2, 12345 some city",
+        description: "This is a Second meetup"
+    }
+]
+
+const HomePage = (props) => {
+ return <MeetupList meetups={props.meetups} />
+}
+
+export async function getStaticProps () {
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        }
+    };
 }
 
 export default HomePage;
